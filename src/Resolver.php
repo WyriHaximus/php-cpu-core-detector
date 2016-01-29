59e2edfe0087147b0a6c2556c15f7793a480ffc8
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-01-29 23:03:03 +0100

    Missing namespace and pass on the command argument

--- src/Resolver.php
+++ src/Resolver.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace WyriHaximus\CpuCoreDetector;
+
 use WyriHaximus\CpuCoreDetector\Core\Affinity\Taskset;
 
 class Resolver
 {
-    public static function resolve($address)
+    public static function resolve($address, $cmd = '')
     {
-        return (new Taskset())->execute($address);
+        return (new Taskset())->execute($address, $cmd);
     }
 }
