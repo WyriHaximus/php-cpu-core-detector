350e3c4bdb7e85b1f79bb734f9389cf918a5616d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-03-07 22:34:40 +0100

    Bit cleaner examples

--- examples/async.php
+++ examples/async.php
@@ -9,7 +9,7 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 $loop = Factory::create();
 
 Detector::detectAsync($loop)->then(function ($result) {
-    var_export($result);
+    echo $result, PHP_EOL;
 });
 
 $loop->run();
--- examples/sync.php
+++ examples/sync.php
@@ -4,4 +4,4 @@ use WyriHaximus\CpuCoreDetector\Detector;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
-var_export(Detector::detect());
+echo Detector::detect(), PHP_EOL;
