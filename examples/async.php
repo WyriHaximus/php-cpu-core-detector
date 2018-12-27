120bda0daf5f982f9fd48db9c0b7d92d728002c3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-12-27 15:08:50 +0100

    Applied new CS rules

--- examples/async.php
+++ examples/async.php
@@ -4,15 +4,15 @@ use React\EventLoop\Factory;
 use WyriHaximus\CpuCoreDetector\Detector;
 use WyriHaximus\CpuCoreDetector\Resolver;
 
-require \dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+require \dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 $loop = Factory::create();
 
 Detector::detectAsync($loop)->then(function ($result): void {
-    echo $result, PHP_EOL;
+    echo $result, \PHP_EOL;
     for ($i = 0; $i < $result; $i++) {
         Resolver::resolve($i, 'uptime')->then(function ($cmd): void {
-            echo $cmd, PHP_EOL;
+            echo $cmd, \PHP_EOL;
         });
     }
 });
